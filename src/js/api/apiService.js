8b00812725d0ae8632b0f31ca300fd490f5088cb
Anna Nikitina <hansa8805@gmail.com>
2021-03-23 10:55:56 +0300

    внесла исправления

--- src/js/api/apiService.js
+++ src/js/api/apiService.js
@@ -5,7 +5,7 @@ const { BASE_URL, API_KEY } = settings;
 export default class ApiService {
     constructor(selector) {
         this.element = document.querySelector(selector);
-        this.currentPage = 1;
+        this.page = 1;
         this.searchQuery = '';
     }
 
@@ -18,18 +18,17 @@ export default class ApiService {
     }
 
     async fetchSearchImages() {
-        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
         const response = await fetch(url);
         const newResponse = await response.json();
-        
         return newResponse.hits;
     }
 
     nextPage() {
-        this.currentPage += 1;
+        this.page += 1;
     }
 
     resetPage() {
-        this.currentPage = 1;
+        this.page = 1;
     }
 }
